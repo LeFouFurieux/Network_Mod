c31be3d56a4f6d7c1aedfcb1ca387c67d34c9802
LeFouFurieux <kimchoiniere@hotmail.com>
2023-07-27 16:30:16 -0400

    Wonky wire

--- src/main/java/com/example/examplemod/custom_block/blue_ethernet.java
+++ src/main/java/com/example/examplemod/custom_block/blue_ethernet.java
@@ -29,13 +29,13 @@ import org.jetbrains.annotations.Nullable;
 
 public class blue_ethernet extends Block {
 
-    public int currentDirectionIndex = 0;
+    public int currentDirectionIndexDest1 = 0;
+    public int currentDirectionIndexDest2 = 0;
+    public int currentWire = 0;
     public static final DirectionProperty source1 = DirectionProperty.create("source1");
+    public static final DirectionProperty source2 = DirectionProperty.create("source2");
     public static final DirectionProperty dest1 = DirectionProperty.create("dest1");
-    public static final BooleanProperty source1_connected = BooleanProperty.create("source1_connected");
-    public static final BooleanProperty dest1_connected = BooleanProperty.create("dest1_connected");
-    public static Block[] blockEthernet;
-    public static Block[] blockWiredConnected;
+    public static final DirectionProperty dest2 = DirectionProperty.create("dest2");
 
     Direction[] possibleDirections = {
           Direction.NORTH, Direction.SOUTH,Direction.WEST,Direction.EAST
@@ -45,51 +45,13 @@ public class blue_ethernet extends Block {
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(source1, Direction.UP)
                 .setValue(dest1, Direction.UP)
-                .setValue(source1_connected,false)
-                .setValue(dest1_connected,false));
-
-        blockEthernet = new Block[]{this, ExampleMod.switch_block.get()};
-        blockWiredConnected = new Block[]{ExampleMod.switch_block.get()};
+                .setValue(source2, Direction.UP)
+                .setValue(dest2, Direction.UP));
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        if(pState.getValue(dest1)==Direction.UP){
-            return switch (pState.getValue(source1) ){
-                case NORTH -> Block.box(7,0,0,9,1,12);
-                case SOUTH -> Block.box(7,0,4,9,1,16);
-                case WEST ->  Block.box(0,0,7,12,1,9);
-                case EAST ->  Block.box(4,0,7,16,1,9);
-                default -> Block.box(7,0,1,9,1,15);
-            };
-        }
-        else{
-            if((pState.getValue(source1) == Direction.NORTH && pState.getValue(dest1) == Direction.SOUTH) ||(pState.getValue(source1) == Direction.SOUTH && pState.getValue(dest1) == Direction.NORTH)) {
-                return Block.box(7,0,0,9,1,16);
-            }
-            else if((pState.getValue(source1) == Direction.WEST && pState.getValue(dest1) == Direction.EAST) || (pState.getValue(source1) == Direction.EAST && pState.getValue(dest1) == Direction.WEST)){
-                return Block.box(0,0,7,16,1,9);
-            }
-
-            switch (pState.getValue(source1)){
-                case NORTH:
-                    if(pState.getValue(dest1) == Direction.WEST){
-                        return Shapes.join(Block.box(7, 0, 0, 9, 0.5, 7.75), Block.box(0, 0, 7, 9, 0.5, 9), BooleanOp.OR);
-                    }
-                    else{
-                        return Shapes.join(Block.box(7, 0, 0, 9, 0.5, 7.75), Block.box(7, 0, 7, 16, 0.5, 9), BooleanOp.OR);
-                    }
-                case SOUTH:
-                    if(pState.getValue(dest1) == Direction.WEST){
-                        return Shapes.join(Block.box(7, 0, 7.75, 9, 0.5, 16), Block.box(0, 0, 7, 9, 0.5, 9), BooleanOp.OR);
-                    }
-                    else{
-                        return Shapes.join(Block.box(7, 0, 7, 9, 0.5, 16), Block.box(7, 0, 7, 16, 0.5, 9), BooleanOp.OR);
-                    }
-            }
             return Block.box(0, 0, 0, 16, 0.5, 16);
-        }
-
     }
 
     @Nullable
@@ -97,10 +59,9 @@ public class blue_ethernet extends Block {
     public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
         for( int i =0; i<possibleDirections.length-1;i++ ){
             if(possibleDirections[i] == blockPlaceContext.getHorizontalDirection().getOpposite()){
-                currentDirectionIndex = i;
+                currentDirectionIndexDest1 = i;
                 break;
             }
-
         }
         return this.defaultBlockState()
                 .setValue(source1,blockPlaceContext.getHorizontalDirection())
@@ -110,17 +71,38 @@ public class blue_ethernet extends Block {
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         if(!pLevel.isClientSide()){
-            do{
-                if(currentDirectionIndex < possibleDirections.length -1){
-                    currentDirectionIndex++;
+            if(pState.getValue(dest2) == Direction.UP && pPlayer.getItemInHand(pHand).getItem() == ExampleMod.BLUE_ETHERNET_ITEM.get()){
+                pLevel.setBlock(pPos,pState
+                        .setValue(source2,pPlayer.getMotionDirection())
+                        .setValue(dest2,pPlayer.getMotionDirection().getOpposite())
+                        ,3);
+            }
+
+            else if(pPlayer.isShiftKeyDown()){
+                if(currentWire < 1){
+                    currentWire++;
                 }
                 else{
-                    currentDirectionIndex = 0;
+                    currentWire =0;
                 }
             }
-            while(possibleDirections[currentDirectionIndex] == pState.getValue(source1));
 
-            pLevel.setBlock(pPos,pState.setValue(dest1,possibleDirections[currentDirectionIndex]),3);
+            else{
+                do{
+                    if(currentDirectionIndexDest1 < possibleDirections.length -1){
+                        currentDirectionIndexDest1++;
+                    }
+                    else{
+                        currentDirectionIndexDest1 = 0;
+                    }
+                }
+                while(possibleDirections[currentDirectionIndexDest1] == pState.getValue(source1));
+
+                switch(currentWire){
+                    case 0 -> pLevel.setBlock(pPos,pState.setValue(dest1,possibleDirections[currentDirectionIndexDest1]),3);
+                    case 1 -> pLevel.setBlock(pPos,pState.setValue(dest2,possibleDirections[currentDirectionIndexDest1]),3);
+                }
+            }
         }
         return InteractionResult.sidedSuccess(pLevel.isClientSide());
     }
@@ -129,8 +111,13 @@ public class blue_ethernet extends Block {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(source1);
         pBuilder.add(dest1);
-        pBuilder.add(source1_connected);
-        pBuilder.add(dest1_connected);
+        pBuilder.add(source2);
+        pBuilder.add(dest2);
     }
 
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        currentWire = 0;
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+    }
 }
